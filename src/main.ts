05a1e07d76b3fc2f709637d97b72116197840052
Avi1997 <avinash9.5thakur@gmail.com>
2020-11-08 23:19:52 +0530

    cors issue resolved

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   try {
-    const PORT = process.env.PORT || 3000;
-    const app = await NestFactory.create(AppModule,{cors:true});
-    app.enableCors()
+    const PORT = process.env.PORT || 8080;
+    const app = await NestFactory.create(AppModule);
+    //app.enableCors()
+    app.use(cors({origin: 'http://localhost:4200'}));
     await app.listen(PORT, () => {
       console.log("SERVER STARTED @PORT ",PORT)
     });
